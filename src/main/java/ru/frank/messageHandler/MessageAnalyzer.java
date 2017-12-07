8056ee8702f38d98320ebf994f5dc4e3dccadce4
VladimirFrank <vladimir.frankov@gmail.com>
2017-12-07 23:13:48 +0700

    Более глубокий анализ входящего сообщения. Бот теперь умеет отправлять видео в ответ.

--- src/main/java/ru/frank/messageHandler/MessageAnalyzer.java
+++ src/main/java/ru/frank/messageHandler/MessageAnalyzer.java
@@ -1,21 +1,78 @@
 package ru.frank.messageHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MessageAnalyzer {
 
     public MessageAnalyzer(){}
 
+    /**
+     *   Коллекция для хранение ответов бота по ключу.
+     */
+    private static Map<String, String> botAnswersList = new HashMap<>();
+
+    static{
+        botAnswersList.put("help", "Бот умеет находить для тебя крутую музыку.%0A" +
+                "%0AСписок команд:%0AПривет%0AКак дела?%0AСкинь трек");
+        botAnswersList.put("привет", "Привет, человек!");
+        botAnswersList.put("как дела?", "ЗАМЕЧАТЕЛЬНО! Хочешь крутую песню, напиши 'скинь трек'");
+    }
+
+    /**
+     *   Коллекция id music videos
+     */
+    private static List<String> musicVideoIdList = new ArrayList<>();
+
+    static{
+        musicVideoIdList.add("456239092");
+        musicVideoIdList.add("456239093");
+        musicVideoIdList.add("456239094");
+        musicVideoIdList.add("456239095");
+        musicVideoIdList.add("456239096");
+        musicVideoIdList.add("456239097");
+        musicVideoIdList.add("456239098");
+        musicVideoIdList.add("456239099");
+        musicVideoIdList.add("456239100");
+        musicVideoIdList.add("456239101");
+        musicVideoIdList.add("456239102");
+        musicVideoIdList.add("456239103");
+        musicVideoIdList.add("456239104");
+        musicVideoIdList.add("456239105");
+
+
+    }
+
+
+
     public String getBotAnswer(String message){
-        String answer = "неизвестен смысл сообщения";
+        message = message.toLowerCase();
+        String answer = "Я тебя не понимаю, человек. Если нужен список команд, напиши 'help'";
+        if(message.contains("help")){
+            answer = botAnswersList.get("help");
+        }
         if(message.contains("как")){
             if(message.contains("дела")){
-                answer = "у меня норм, а у тебя?";
+                answer = botAnswersList.get("как дела?");
             }
         } else if(message.contains("привет")){
-            answer = "привет пёс!";
+            answer = botAnswersList.get("привет");
+        } else if(message.contains("трек")){
+            answer = "Вот твой крутой трек!&attachment=video172635477_" + getRandomMusicVideo();
         }
         answer = answer.replace(" ", "+");
         return answer;
     }
 
 
+    /**
+     * Генерирует случайное число в пределах от 0 до размера массива (хранящего id music videos).
+     * @return String;
+     */
+    private String getRandomMusicVideo(){
+        int videoIndex = (int) (Math.random() * musicVideoIdList.size());
+        return musicVideoIdList.get(videoIndex);
+    }
 }
--- src/main/java/ru/frank/messageHandler/VkMessageParser.java
+++ src/main/java/ru/frank/messageHandler/VkMessageParser.java
@@ -8,19 +8,22 @@ import com.google.gson.JsonParser;
  *  Класс парсит json поступивший со стороны клиента.
  *  Структура входящего запроса:
  * {
-        "type":"message_new",
-        "object":{
-            "id":694,
-            "date":1499441696,
-            "out":0,
-            "user_id":123456,
-            "read_state":0,
-            "title":" ... ",
-            "body":"start"
-        },
-        "group_id":1,
-        "secret":"sjr948dff3kjnfd3"
-        }
+ *       "type":"message_new",
+ *       "object":{
+ *           "id":694,
+ *           "date":1499441696,
+ *           "out":0,
+ *           "user_id":123456,
+ *           "read_state":0,
+ *           "title":" ... ",
+ *          "body":"start"
+ *       },
+ *       "group_id":1,
+ *       "secret":"sjr948dff3kjnfd3"
+ *       }
+ *  два основных поля:
+ *  user_id - для индентификации пользователя, кому будет отправлен ответ
+ *  body - входящее сообщение, для дальнейшего анализа
  */
 public class VkMessageParser {
 
@@ -32,6 +35,12 @@ public class VkMessageParser {
     private String message;
     private String userId;
 
+    /**
+     * При инстанцировании класса, присваиваются значения полям @user_id и @body
+     * на основании которых далее будет происходить анализ входящего сообщения и
+     * формирование ответа пользователю.
+     * @param incomingJson - входящее сообщение пользователя для парсинга.
+     */
     public VkMessageParser(String incomingJson){
         parser = new JsonParser();
         jsonElement = parser.parse(incomingJson);
@@ -41,22 +50,12 @@ public class VkMessageParser {
         this.userId = childJsonObject.get("user_id").getAsString();
     }
 
-
-
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
